bc69a3866661beb9e051fd81162606a8f6eeee50
shriaviator <32628818+shriaviator@users.noreply.github.com>
2021-01-09 08:49:41 +0530

    base changed

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 const { description } = require("../../package");
 
 module.exports = {
-  base: "/shriaviator/",
+  base: "/vuepress-project-one/",
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
